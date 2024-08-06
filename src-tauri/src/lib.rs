1b6203c63749ba663f5233efc1342f4debeb419b
Jacob Bolda <me@jacobbolda.com>
2024-08-06 11:30:44 -0500

    Initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,61 @@
+use tauri_specta::Event;
+
+// demo command
+#[tauri::command]
+#[specta::specta]
+fn greet(app: tauri::AppHandle, name: &str) -> String {
+    DemoEvent("Hello from Rust 🦀".to_string()).emit(&app).ok();
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+// demo event
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, specta::Type, Event)]
+pub struct DemoEvent(String);
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    #[cfg(debug_assertions)]
+    let devtools = tauri_plugin_devtools::init();
+    let mut builder = tauri::Builder::default();
+
+    let specta_builder = tauri_specta::Builder::<tauri::Wry>::new()
+        .commands(tauri_specta::collect_commands![greet])
+        .events(tauri_specta::collect_events![crate::DemoEvent]);
+
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+
+    #[cfg(all(debug_assertions, not(mobile)))]
+    specta_builder
+        .export(
+            specta_typescript::Typescript::default()
+                .formatter(specta_typescript::formatter::prettier)
+                .header("/* eslint-disable */\n"),
+            "../src/bindings.ts",
+        )
+        .expect("failed to export typescript bindings");
+
+    builder
+        .plugin(tauri_plugin_shell::init())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(specta_builder.invoke_handler())
+        .setup(move |app| {
+            specta_builder.mount_events(app);
+
+            // listen to demo event
+            DemoEvent::listen(app, |event| {
+                dbg!(event.payload);
+            });
+
+            // dispatch demo event
+            DemoEvent("Hello from Rust 🦀".to_string()).emit(app).ok();
+            // /dispatch demo event
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
